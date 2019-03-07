dee7c9d3e9b6c404840e04d7d1c2081c7cbd476a
binnyang179 <binyang179@163.com>
2019-03-07 21:30:04 +0800

    sorry, only did little work.

--- src/ImportTxtToMysql.java
+++ src/ImportTxtToMysql.java
@@ -6,28 +6,25 @@ public class ImportTxtToMysql {
 
 
     public static void main(String[] args) throws Exception {
-
         BufferedReader reader;
-
 
         MySQLAccess dao = new MySQLAccess();
 
+        dao.initDB();
         dao.createDB();
-
-
 
         try {
             reader = new BufferedReader(new FileReader(
+                    //这儿你的改成你的ip.txt的目录
                     "/home/crab179/Documents/ip.txt"));
             String line = reader.readLine();
+            System.out.println("数据较多，请等待，结束时会有提示");
             while (line != null) {
-
-
-                dao.insertIntoDatabase(line.substring(0,16), line.substring(17, 32), line.substring(33));
-
+                dao.insertIntoDatabase(line.substring(0,16), line.substring(16, 32), line.substring(32));
                 line = reader.readLine();
             }
             reader.close();
+            System.out.println("录入成功,现在可以查看了");
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/MySQLAccess.java
+++ src/MySQLAccess.java
@@ -1,36 +1,42 @@
-import java.security.KeyStore;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Date;
-
 public class MySQLAccess {
     private Connection connect = null;
     private Statement statement = null;
-    private ResultSet resultSet = null;
 
-
-    public void createDB() throws SQLException, ClassNotFoundException {
+    public void initDB() {
         try {
+
             // This will load the MySQL driver, each DB has its own driver
             Class.forName("com.mysql.cj.jdbc.Driver");
             // Setup the connection with the DB
 
             connect = DriverManager.getConnection("jdbc:mysql://10.2.8.73:3306/","root","123456");
-
             // Statements allow to issue SQL queries to the database
             statement = connect.createStatement();
 
+            System.out.println("Initial success");
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public void createDB() throws SQLException {
+        try {
             String a = "drop database if exists IPAddressInquiry;";
             String b = "create database IPAddressInquiry;";
 
             String c = "create table IPAddressInquiry.ipaddress (\n" +
                     "  ipa varchar(20),\n" +
                     "  ipb varchar(20),\n" +
-                    "  address varchar(100)\n" +
+                    "  address varchar(120)\n" +
                     ");";
 
             String d = "use IPAddressInquiry;";
@@ -48,8 +54,6 @@ public class MySQLAccess {
 
         } catch (Exception e) {
             throw e;
-        } finally {
-            close();
         }
 
 
@@ -59,25 +63,12 @@ public class MySQLAccess {
     public void insertIntoDatabase(String ipa, String ipb, String address ) throws Exception {
 
         try {
-            // This will load the MySQL driver, each DB has its own driver
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            // Setup the connection with the DB
-            connect = DriverManager.getConnection("jdbc:mysql://10.2.8.73:3306/","root","123456");
-
-            // Statements allow to issue SQL queries to the database
-            statement = connect.createStatement();
-
-
-            statement.execute("insert into IPAddressInquiry.ipaddress values(" + "'" + ipa + "'" +"," + "'" + ipb + "'" + "," + "'" + address + "'" + ");");
-
-            System.out.println("insert success");
-
+            String sql = "insert into IPAddressInquiry.ipaddress values(" + "'" + ipa + "'" + "," + "'" + ipb + "'" + "," + "'" + address + "');";
+            statement.execute(sql);
         } catch (Exception e) {
             System.out.println("insert failed");
             statement.execute("drop database if exists IPAddressInquiry;");
             throw e;
-        } finally {
-            close();
         }
 
     }
@@ -85,9 +76,6 @@ public class MySQLAccess {
     // You need to close the resultSet
     public void close() {
         try {
-            if (resultSet != null) {
-                resultSet.close();
-            }
 
             if (statement != null) {
                 statement.close();
